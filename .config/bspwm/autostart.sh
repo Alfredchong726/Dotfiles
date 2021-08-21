10bbd33dc56c8960e7810895d7e80d58fc602daf
Alfredchong260 <alfredchong260@gmail.com>
2021-08-21 14:40:21 +0800

    adding bspwm config

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+$HOME/.config/polybar/launch.sh &
+
+run sxhkd -c ~/.config/sxhkd/sxhkdrc &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+blueberry-tray &
+picom --config $HOME/.config/picom/picom.conf &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+run volumeicon &
+nitrogen --random --set-tiled &
